e341c8c120e75d3c4cdc5ccbc398da8b32533e1e
Jarret <12jpiper@gmail.com>
2020-05-03 04:24:26 -0500

    Changes to settings and update to index.html

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'CHANGE_ME'
+SECRET_KEY = 'sDAADSFFHHdxASDFcvzxcv09831248094231098!@#$%^^&_)((*&^%%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
