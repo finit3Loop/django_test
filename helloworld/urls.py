4f8c7d6e1bb3160d0a34eec0fb0fc0c38207677b
Jarret <12jpiper@gmail.com>
2020-06-25 00:34:22 -0500

    Import passWord class into urls.py

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from helloworld import views
-from helloworld.views import get_password
+from helloworld.views import passWord
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
