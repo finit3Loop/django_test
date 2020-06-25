eb26150c25260e552de86a748c11d5530c5236d6
Jarret <12jpiper@gmail.com>
2020-06-25 00:22:01 -0500

    Corrected urlpatterns in urls.py and views.py

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
-   url(r'^', include('helloworld.urls')), 
+   url(r'^', include('helloworld.urls')),
+   url(r'^hasher/$', include('helloworld.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
